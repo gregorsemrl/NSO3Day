a2a31f6ab2ab266423069baca4d444324cf6fbf5
gsemrl <gsemrl@nil.com>
2022-08-26 10:26:46 +0200

    Python package + additional documentation

---
+++ examples/dns-snmp-py/python/dns_snmp_py/main.py
@@ -0,0 +1,83 @@
+# -*- mode: python; python-indent: 4 -*-
+import ncs
+from ncs.application import Service
+
+
+# ------------------------
+# SERVICE CALLBACK EXAMPLE
+# ------------------------
+class ServiceCallbacks(Service):
+
+    # The create() callback is invoked inside NCS FASTMAP and
+    # must always exist.
+    @Service.create
+    def cb_create(self, tctx, root, service, proplist):
+        self.log.info('Service create(service=', service._path, ')')
+
+        # Normally, this would be calculated, but here for the example
+        # we will just copy the values from the service model parameters.
+        # Note: Dashes "-" are converted to underscore "_" in Python.
+
+        dns_server_ip = service.dns_server
+        snmp_server_ip = service.snmp_server
+        acl_number = service.acl_number
+        community_str = service.community_string
+
+        # Apply variables to the template
+        vars = ncs.template.Variables()
+        vars.add('DNS_SERVER', dns_server_ip)
+        vars.add('SNMP_SERVER', snmp_server_ip)
+        vars.add('ACL_NUMBER', acl_number)
+        vars.add('COMMUNITY_STRING', community_str)
+
+        template = ncs.template.Template(service)
+        template.apply('dns-snmp-py-template', vars)
+
+    # The pre_modification() and post_modification() callbacks are optional,
+    # and are invoked outside FASTMAP. pre_modification() is invoked before
+    # create, update, or delete of the service, as indicated by the enum
+    # ncs_service_operation op parameter. Conversely
+    # post_modification() is invoked after create, update, or delete
+    # of the service. These functions can be useful e.g. for
+    # allocations that should be stored and existing also when the
+    # service instance is removed.
+
+    # @Service.pre_lock_create
+    # def cb_pre_lock_create(self, tctx, root, service, proplist):
+    #     self.log.info('Service plcreate(service=', service._path, ')')
+
+    # @Service.pre_modification
+    # def cb_pre_modification(self, tctx, op, kp, root, proplist):
+    #     self.log.info('Service premod(service=', kp, ')')
+
+    # @Service.post_modification
+    # def cb_post_modification(self, tctx, op, kp, root, proplist):
+    #     self.log.info('Service postmod(service=', kp, ')')
+
+
+# ---------------------------------------------
+# COMPONENT THREAD THAT WILL BE STARTED BY NCS.
+# ---------------------------------------------
+class Main(ncs.application.Application):
+    def setup(self):
+        # The application class sets up logging for us. It is accessible
+        # through 'self.log' and is a ncs.log.Log instance.
+        self.log.info('Main RUNNING')
+
+        # Service callbacks require a registration for a 'service point',
+        # as specified in the corresponding data model.
+        #
+        self.register_service('dns-snmp-py', ServiceCallbacks)
+
+        # If we registered any callback(s) above, the Application class
+        # took care of creating a daemon (related to the service/action point).
+
+        # When this setup method is finished, all registrations are
+        # considered done and the application is 'started'.
+
+    def teardown(self):
+        # When the application is finished (which would happen if NCS went
+        # down, packages were reloaded or some error occurred) this teardown
+        # method will be called.
+
+        self.log.info('Main FINISHED')
